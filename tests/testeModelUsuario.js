785deb8cf3640deef4910ea42f6fe5d953252012
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-05-16 08:17:38 -0300

    Nova branche para desenvolver a feature de api

--- tests/testeModelUsuario.js
+++ tests/testeModelUsuario.js
@@ -1,7 +1,7 @@
 const { Usuario, sequelize } = require('../models');
 
 //Encontrar todos
-// Publicacao.findAll().then(result => console.log(result.toJSON()));
+// Usuario.findAll().then(result => console.log(result.toJSON()));
 
 //Encontrar pela chave primária
 Usuario.findByPk(2, {include: "publicacoes"}).then(result => {
