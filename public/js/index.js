80fd29ffda7f4b328a68d1b08874acdc9aa84219
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-06-01 11:34:21 -0300

    Realizado validação do formulário de cadastro. Aula 01/06/2022

--- public/js/index.js
+++ public/js/index.js
@@ -2,20 +2,32 @@
 
 // Capturar o elemento input do email
 let inputEmail = document.querySelector('#registro > form input[type=email]');
-console.log(inputEmail);
+let inputSenha = document.querySelector('#registro > form input[type=password]');
+let inputFile = document.querySelector('#registro > form input[type=file]');
+
 
 //Associar ao evento "perdeu o foco", uma função
 // Avisar ao usuário que o campo de email foi deixado em branco.
-const verificaCampoPreenchido = () => {
-    if(inputEmail.value == "") {
-        inputEmail.style.outline = "4px solid #663300";
-        inputEmail.setAttribute("placeholder", "Digite o e-mail");
+const verificaCampoPreenchido = (event) => {
+    console.log(event);
+    
+    if(event.target.value == "") {
+        event.target.style.outline = "4px solid #663300";
+        event.target.setAttribute("placeholder", `Preencha o campo: ${event.target.name}`);
     } else{
-        inputEmail.style.outline = "none";
-        inputEmail.setAttribute("placeholder", "")
+        event.target.style.outline = "none";
+        event.target.setAttribute("placeholder", "")
 
     }
 }
 
+const onFileChange = event => {
+    let img = document.getElementById("output");
+    img.src = URL.createObjectURL(event.target.files[0]);
+   /*  img.style.marginTop = "20px" */
+}
+
 inputEmail.addEventListener("blur", verificaCampoPreenchido);
+inputSenha.addEventListener("blur", verificaCampoPreenchido);
+inputFile.addEventListener("change", onFileChange)
 
